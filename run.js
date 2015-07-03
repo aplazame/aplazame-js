9b8bc54207ebce24ad0068c87b0c876e8719b692
Jesús Germade <jesus@germade.es>
2015-07-03 20:58:54 +0200

    html object close to be done

--- run.js
+++ run.js
@@ -5,19 +5,32 @@ function htmlToJs (html) {
   return html.replace(/\n/g, '').replace(/\"/g, '\\\"').replace(/\>\s+\</g, '><');
 }
 
-var cmd = {
-  build: function () {
-    var fs = require('fs'),
-        UglifyJS = require("uglify-js"),
-        aplazameMin = UglifyJS.minify("src/main.js").code,
-        iframeSrc = htmlToJs( fs.readFileSync('src/iframe.html', { encoding: 'utf8' }) );
+var colors = require('colors'),
+    cmd = {
+      build: function () {
+        var fs = require('fs'),
+            UglifyJS = require("uglify-js"),
+            aplazameMin = UglifyJS.minify("src/main.js").code,
+            iframeSrc = htmlToJs( fs.readFileSync('src/iframe.html', { encoding: 'utf8' }) );
 
-    aplazameMin = aplazameMin.replace('::iframeHtml::', iframeSrc );
-    fs.writeFileSync('aplazame.js', aplazameMin );
-  },
-  test: function () {
+        aplazameMin = aplazameMin.replace('::iframeHtml::', iframeSrc );
+        fs.writeFileSync('aplazame.js', aplazameMin );
 
-  }
-};
+        console.log('aplazame.js', 'updated'.green);
+      },
+      watch: function () {
+        var watch = require('node-watch');
+
+        watch(['src'], function(filename) {
+          console.log(filename.yellow, 'changed');
+          cmd.build();
+        });
+
+        console.log('watching...'.blue, 'press Ctrl-C to stop'.yellow);
+      },
+      test: function () {
+
+      }
+    };
 
 process.argv[2] && cmd[process.argv[2]] && cmd[process.argv[2]]();
--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 (function (root) {
   'use strict';
 
-  function button () {}
-
   var iframeHtml = '::iframeHtml::',
       iframeStyle = {
         position: 'fixed',
@@ -13,15 +11,75 @@
         height: '100%'
       };
 
+  // utility functions
+
+  var arrayShift = [].shift;
+  function extend () {
+      var dest = arrayShift.call(arguments),
+        src = arrayShift.call(arguments),
+        key;
+
+    while( src ) {
+      for( key in src) {
+        dest[key] = src[key];
+      }
+      src = arrayShift.call(arguments);
+    }
+
+    return dest;
+  }
+
+  function html (url, options) {
+    options = options || {};
+    options.headers = options.headers || {};
+
+    var request = null,
+        on = { fulfill: [], reject: [] };
+
+    try { // Firefox, Opera 8.0+, Safari
+        request = new XMLHttpRequest();
+    } catch (e) { // Internet Explorer
+        try { request = new ActiveXObject("Msxml2.XMLHTTP"); }
+        catch (e) { request = new ActiveXObject("Microsoft.XMLHTTP"); }
+    }
+    if( request === null ) { throw "Browser does not support HTTP Request"; }
+
+    request.open( options.method.toUpperCase(), url );
+
+    for( key in options.headers ) {
+        request.setRequestHeader( toTitleSlug(key), options.headers[key] );
+    }
+
+    request.onreadystatechange = function(){
+        if( request.readyState === 'complete' || request.readyState === 4 ) {
+
+        }
+    };
+
+    return {
+      success: function (onFulfill, onReject) {
+        on.fulfill.push(onFulfill);
+        onReject instanceof Function && on.reject.push(onreject);
+      },
+      error: function (onReject) {
+        on.reject.push(onReject);
+      }
+    };
+  };
+
+  // aplazame methods
+
+  function button () {}
+
   function checkout () {
     var iframe = document.createElement('iframe');
     iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(iframeHtml);
-    for( var key in iframeStyle ) {
-      iframe.style[key] = iframeStyle[key];
-    }
+    extend(iframe.style, iframeStyle);
     iframe.frameBorder = '0';
     document.body.appendChild(iframe);
   }
+
+  // globalizing aplazame object
 
   root.aplazame = {
     checkout: checkout,

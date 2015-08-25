aa44e0444f928853e0647ec6214e0e1c66dbb9cf
calvinpy <dani@aplazame.com>
2015-08-26 00:03:57 +0200

    deploy when branch

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-case $DRONE_BRANCH in
-    release)
-        echo release deployment
+# [DOCKER] Install deployment requirements
+sudo pip install fabric
 
-        # [DOCKER] Install deployment requirements
-        sudo pip install fabric
+# [PRODUCTION] make echo
+fab prod echo
 
-        # [PRODUCTION] make echo
-        fab prod echo
-
-        # [PRODUCTION] deploy
-        # make deploy host=prod
-        fab prod deploy
-
-        ;;
-
-    *)
-        echo *$DRONE_BRANCH* ci
-        ;;
-esac
+# [PRODUCTION] deploy
+# make deploy host=prod
+fab prod deploy

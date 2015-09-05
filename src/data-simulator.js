1bec6f9b65191a9cec32500f7e2de1ba2ba5ee3f
Jesús Germade <jesus@germade.es>
2015-09-06 00:51:14 +0200

    widget aplazame branding and auto-resizing

--- src/data-simulator.js
+++ src/data-simulator.js
@@ -4,7 +4,24 @@ var aplazame = require('./aplazame'),
 if( simulators.length ) {
 
   var http = require('./http'),
-      _ = require('./utils');
+      _ = require('./utils'),
+      iframes = [];
+
+  _.listen(window, 'message', function (e) {
+    var message = e.data;
+
+
+    if( message.aplazame === 'simulator' ) {
+      console.log('aplazame-simulator:message', message, e.source);
+
+      iframes.forEach(function (iframe) {
+        console.log('aplazame-simulator:iframe', iframe, e.source, iframe.contentWindow === e.source );
+        if( iframe.contentWindow === e.source ) {
+          iframe.style.height = message.data.height + 'px';
+        }
+      });
+    }
+  });
 
   [].forEach.call(simulators, function (simulator) {
     'use strict';
@@ -26,13 +43,15 @@ if( simulators.length ) {
       console.log('simulator', simulatorParams, choices);
 
       http('dist/widgets/simulator/simulator.html').then(function (response) {
-        var iframe = _.getIFrame();
+        var iframe = _.getIFrame({
+          width: '100%'
+        });
+        iframes.push(iframe);
         simulator.appendChild(iframe);
         _.writeIframe(iframe,
           response.data
             .replace(/<head\>/, '<head><base href="' + aplazame.baseUrl() + '" />')
             .replace(/\/\/ choices = \[\];/, 'choices = ' + JSON.stringify(choices) + ';')
-            // .replace(/\/\/ choices = \[\];/, 'choices = ' + JSON.stringify(choices, null, '\t') + ';')
         );
       });
     });
--- src/widgets/simulator/simulator.js
+++ src/widgets/simulator/simulator.js
@@ -4,15 +4,7 @@ var _ = require('../../utils');
 _.template.lookup();
 
 var main = document.getElementById('main'),
-    choice, choices = window.choices;
-
-function showText () {
-  main.innerHTML = _.template('info', choice);
-}
-
-function showChoices () {
-  main.innerHTML = _.template('choices', { choices: choices });
-}
+    selectedChoice, choices = window.choices;
 
 function getAmount (amount) {
   var prefix = '';
@@ -32,18 +24,31 @@ function getAmount (amount) {
   return prefix + ('' + amount).replace(/..$/, ',$&');
 }
 
-function setChoice (data) {
-  choice = _.extend({ getAmount: getAmount }, data );
+function showText () {
+  main.innerHTML = _.template('info', {
+    getAmount: getAmount,
+    choice: selectedChoice
+  });
+}
+
+function showChoices () {
+  main.innerHTML = _.template('choices', { selectedChoice: selectedChoice, choices: choices });
+}
+
+function setChoice (choice) {
+  selectedChoice = choice;
   return choice;
 }
 
-setChoice( choices.reduce(function (prev, choice) {
+function maxInstalments (prev, choice) {
   if( prev === null ) {
     return choice;
   } else {
     return choice.num_instalments > prev.num_instalments ? choice : prev;
   }
-}, null) );
+}
+
+setChoice( choices.reduce(maxInstalments, null) );
 
 _.listen(main, 'click', function (e) {
   var action = e.target.getAttribute('data-action');
@@ -56,10 +61,26 @@ _.listen(main, 'click', function (e) {
       showChoices();
       break;
     case 'selectChoice':
-      setChoice(choices[ Number(e.target.getAttribute('data-choice')) ]);
+      setChoice( choices[ Number(e.target.getAttribute('data-choice')) ] );
       showText();
       break;
   }
 });
 
 showText();
+
+function emitSize () {
+  setTimeout(function () {
+    parent.window.postMessage({
+      aplazame: 'simulator',
+      event: 'resize',
+      data: {
+        width: document.body.clientWidth,
+        height: document.body.clientHeight
+      }
+    }, '*');
+  },1);
+}
+
+_.listen(window, 'load', emitSize);
+_.listen(window, 'resize', emitSize);

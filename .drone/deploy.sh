c1f1633c5838d1f01965eb0d80f0b9dd371f5767
calvinpy <dani@aplazame.com>
2015-09-08 00:39:45 +0200

    deploy to debug

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -3,9 +3,17 @@
 # [DOCKER] Install deployment requirements
 sudo pip install fabric
 
-# [PRODUCTION] make echo
-fab prod echo
 
-# [PRODUCTION] deploy
-# make deploy host=prod
-fab prod deploy
+case $DRONE_BRANCH in
+    master)
+        echo master deploy to debug
+        fab prod deploy:$DEBUG_PATH,master
+        fab prod echo:$DEBUG_PATH
+        ;;
+
+    release)
+        echo release deploy to production
+        fab prod deploy:$RELEASE_PATH,release
+        fab prod echo:$RELEASE_PATH
+        ;;
+esac

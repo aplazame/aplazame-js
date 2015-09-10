803d2a6e64152af76af6a525af18d9bf6a46f05a
Jesús Germade <jesus@germade.es>
2015-09-10 18:33:13 +0200

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.31';
+module.exports = '0.0.32';
 },{}],2:[function(require,module,exports){
 'use strict'; // jshint ignore:line
 
@@ -94,13 +94,13 @@ function button (options) {
       options.watching = true;
 
       setInterval(function () {
-        // console.log('checking price', options.amount, getCartPrice() );
+        console.log('checking price', options.amount, getCartPrice() );
         if( options.amount !== getCartPrice() ) {
           options.amount = getCartPrice();
           options.forceUpdate = true;
           button(options);
         }
-      }, 400);
+      }, 4000);
     }
   }
 

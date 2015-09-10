1ea3a8dc854072889893c76893df9800dc880461
Jesús Germade <jesus@germade.es>
2015-09-10 21:54:36 +0200

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.39';
+module.exports = '0.0.40';
 },{}],2:[function(require,module,exports){
 'use strict'; // jshint ignore:line
 
@@ -177,11 +177,9 @@ function button (options) {
   apiGet('checkout/button', { params: params })
     .then(function () {
       var elms = elements.slice();
-      setTimeout(function () {
-        elms.forEach(function (el) {
-          el.style.display = el.__display;
-        });
-      }, 2000);
+      elms.forEach(function (el) {
+        el.style.display = el.__display;
+      });
     });
 
   elements.forEach(function (el) {

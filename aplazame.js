9de2eeaf654221b78ebbab94fb3f62137564ffe5
Jesús Germade <jesus@germade.es>
2015-09-10 18:47:16 +0200

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.35';
+module.exports = '0.0.36';
 },{}],2:[function(require,module,exports){
 'use strict'; // jshint ignore:line
 
@@ -93,6 +93,8 @@ function button (options) {
     if( !options.watching ) {
       options.watching = true;
       options.lastPrice = options.amount;
+
+      console.debug('total price watching');
 
       setInterval(function () {
         console.log('checking price', options.amount, getCartPrice() );

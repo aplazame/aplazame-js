a0b1f970643fb36b8bf86ba05d431ab9b63ecc26
Jesús Germade <jesus@germade.es>
2015-09-10 18:53:06 +0200

    faster checker and removed console logs

--- src/aplazame.js
+++ src/aplazame.js
@@ -91,17 +91,17 @@ function button (options) {
       button.watching = true;
       options.lastPrice = options.amount;
 
-      console.debug('total price watching');
+      // console.debug('total price watching');
 
       setInterval(function () {
-        console.log('checking price', options.amount, getCartPrice() );
+        // console.log('checking price', options.amount, getCartPrice() );
         if( getCartPrice() !== options.lastPrice ) {
           options.amount = getCartPrice();
           options.lastPrice = options.amount;
           options.forceUpdate = true;
           button(options);
         }
-      }, 4000);
+      }, 400);
     }
   }
 

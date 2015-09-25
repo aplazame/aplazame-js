b95913f833eeda992edde6ccbdfefe6ac3dd683e
Jesús Germade <jesus@germade.es>
2015-09-25 11:40:26 +0200

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.45';
+module.exports = '0.0.46';
 },{}],2:[function(require,module,exports){
 'use strict'; // jshint ignore:line
 
@@ -211,8 +211,14 @@ function checkout (options) {
           'z-index': 2147483647
         });
 
+    // iframe.setAttribute('allowtransparency', 'true');
+    // iframe.setAttribute('allowfullscreen', 'true');
+    // iframe.setAttribute('sandbox', 'allow-scripts allow-pointer-lock allow-same-origin allow-popups allow-forms');
+
     document.body.appendChild(iframe);
-    _.writeIframe(iframe, iframeHtml);
+    iframe.src = baseUrl + 'iframe.html?' + new Date().getTime();
+    // iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(iframeHtml);
+    // _.writeIframe(iframe, iframeHtml);
 
     if( !options.merchant ) {
       throw new Error('missing merchant parameters');

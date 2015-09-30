662c209cc31662b8a260fb023f6e996c3145d516
Jesús Germade <jesus@germade.es>
2015-09-30 16:27:59 +0200

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.58';
+module.exports = '0.0.59';
 },{}],2:[function(require,module,exports){
 'use strict';
 
@@ -283,15 +283,15 @@ function checkout (options) {
             } ).then(function (response) {
               e.source.postMessage({
                 aplazame: 'checkout',
-                event: 'success',
-                result: 'ack',
+                event: 'confirmation',
+                result: 'success',
                 response: response
               }, '*');
             }, function () {
               e.source.postMessage({
                 aplazame: 'checkout',
-                event: 'success',
-                result: 'ko',
+                event: 'confirmation',
+                result: 'error',
                 response: response
               }, '*');
             });

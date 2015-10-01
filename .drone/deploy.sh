f230e6365e5b8598a4c383cb914eb2f00cf9a385
calvinpy <dani@aplazame.com>
2015-10-01 23:49:48 +0200

    deploy and build debug

--- .drone/deploy.sh
+++ .drone/deploy.sh
@@ -8,12 +8,11 @@ case $DRONE_BRANCH in
     master)
         echo master deploy to debug
         fab prod deploy:$DEBUG_PATH,master
-        fab prod echo:$DEBUG_PATH
+        fab prod make:$DEBUG_PATH,build
         ;;
 
     release)
         echo release deploy to production
         fab prod deploy:$RELEASE_PATH,release
-        fab prod echo:$RELEASE_PATH
         ;;
 esac

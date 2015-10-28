fb23d389fec5c22b00280dc1961a1f848792f4a2
Jesús Germade <jesus@germade.es>
2015-10-28 16:26:56 +0100

    avoiding iframe[src]

--- src/data-simulator.js
+++ src/data-simulator.js
@@ -73,8 +73,13 @@ function widgetsLookup (element) {
             width: '100%'
           });
           iframes.push(iframe);
-          iframe.src = api.baseUrl + 'widgets/simulator/simulator.html?' + now;
+          // iframe.src = api.baseUrl + 'widgets/simulator/simulator.html?' + now;
           simulator.appendChild(iframe);
+
+          _.writeIframe(iframe,
+            response.data
+              .replace(/<head\>/, '<head><base href="' + api.baseUrl + 'widgets/simulator/" />')
+          );
 
           // _.writeIframe(iframe,
           //   response.data

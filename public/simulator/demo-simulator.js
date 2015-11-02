8849c21ca6b93e325d466ac3fcafcff0687ae162
Jesús Germade <jesus@germade.es>
2015-11-02 18:39:41 +0100

    updating built versions

--- public/simulator/demo-simulator.js
+++ public/simulator/demo-simulator.js
@@ -1,1 +1,592 @@
-undefined
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* globals aplazame */
+
+var _ = require('../src/tools/tools');
+
+_.template.lookup();
+
+var form = document.querySelector('form[data-submit="updateValues"]'),
+    results = document.querySelector('.demo-simulator-results'),
+    amount = form.querySelector('input[name="amount"]');
+
+amount.value = localStorage.getItem('demo-simulator-amount');
+
+function getCents(value) {
+  var values = value.split(/[,.]/);
+  return '' + ( Number( values[0] ? 100*values[0] : 0 ) + Number( values[1] || 0 ) );
+}
+
+_.listen(form, 'submit', function (e) {
+  console.log('gogogo!');
+  e.preventDefault();
+
+  localStorage.setItem('demo-simulator-amount', amount.value);
+
+  var currentAmount = getCents( amount.value );
+
+  results.innerHTML = '<div class="loading">cargando...</div>';
+
+  aplazame.simulator(currentAmount, function (choices) {
+    console.log('simulator', currentAmount, choices);
+    results.innerHTML = _.template('results', {
+      getAmount: _.getAmount,
+      choices: choices,
+      months: function (m) {
+        return m > 1 ? 'meses' : 'mes';
+      }
+    });
+  });
+});
+
+},{"../src/tools/tools":6}],2:[function(require,module,exports){
+
+if( !Element.prototype.matchesSelector ) {
+  Element.prototype.matchesSelector = (
+    Element.prototype.webkitMatchesSelector ||
+    Element.prototype.mozMatchesSelector ||
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.oMatchesSelector
+  );
+}
+
+function _isType (type) {
+    return function (o) {
+        return (typeof o === type);
+    };
+}
+
+function _instanceOf (_constructor) {
+    return function (o) {
+        return ( o instanceof _constructor );
+    };
+}
+
+var _isObject = _isType('object'),
+    _isFunction = _isType('function'),
+    _isString = _isType('string'),
+    _isNumber = _isType('number'),
+    _isArray = Array.isArray || _instanceOf(Array),
+    _isDate = _instanceOf(Date),
+    _isRegExp = _instanceOf(RegExp),
+    _isElement = function(o) {
+      return o && o.nodeType === 1;
+    };
+
+if( window.attachEvent && !window.HTMLElement.prototype.addEventListener ) {
+  window.HTMLElement.prototype.addEventListener = function (eventName, listener) {
+    this.attachEvent('on' + eventName, listener);
+  };
+}
+
+function listen (element, eventName, listener) {
+  if( element instanceof Array ) {
+    for( var i = 0, n = element.length ; i < n ; i++ ) {
+      element[i].addEventListener(eventName, listener, false);
+    }
+    return;
+  }
+  element.addEventListener(eventName, listener, false);
+}
+
+// var listen = window.addEventListener ? function (element, eventName, listener) {
+//   if( element instanceof Array ) {
+//     for( var i = 0, n = element.length ; i < n ; i++ ) {
+//       element[i].addEventListener(eventName, listener, false);
+//     }
+//     return;
+//   }
+//   element.addEventListener(eventName, listener, false);
+// } : ( window.attachEvent && function (element, eventName, listener) {
+//   if( element instanceof Array ) {
+//     for( var i = 0, n = element.length ; i < n ; i++ ) {
+//       element[i].addEventListener(eventName, listener, false);
+//     }
+//     return;
+//   }
+//   element.attachEvent('on' + eventName, listener);
+// } );
+
+
+if( !window.HTMLElement.prototype.addEventListener ) {
+  throw new Error('Your Browser does not support events');
+}
+
+function once (fn) {
+  var done;
+  return function () {
+    if( !done ) {
+      done = true;
+      return fn.apply(this, arguments);
+    }
+  };
+}
+
+function docReady (callback) {
+  if( document.readyState === 'complete' ) {
+    callback();
+  } else {
+    listen(window, 'load', callback);
+  }
+}
+
+function replaceKeys (tmpl, keys) {
+  return keys ? tmpl.replace(/\{\{([^\}]+)\}\}/g, function (match, key) {
+    return keys[key];
+  }) : function (ks) {
+    return replaceKeys(tmpl, ks);
+  };
+}
+
+var arrayShift = [].shift;
+function extend () {
+    var dest = arrayShift.call(arguments),
+      src = arrayShift.call(arguments),
+      key;
+
+  while( src ) {
+    for( key in src) {
+      dest[key] = src[key];
+    }
+    src = arrayShift.call(arguments);
+  }
+
+  return dest;
+}
+
+function merge () {
+    var dest = arrayShift.call(arguments),
+        src = arrayShift.call(arguments),
+        key;
+
+    while( src ) {
+
+        if( typeof dest !== typeof src ) {
+            dest = _isArray(src) ? [] : ( _isObject(src) ? {} : src );
+        }
+
+        if( _isObject(src) ) {
+
+            for( key in src ) {
+                if( src[key] !== undefined ) {
+                    if( typeof dest[key] !== typeof src[key] ) {
+                        dest[key] = merge(undefined, src[key]);
+                    } else if( _isArray(dest[key]) ) {
+                        [].push.apply(dest[key], src[key]);
+                    } else if( _isObject(dest[key]) ) {
+                        dest[key] = merge(dest[key], src[key]);
+                    } else {
+                        dest[key] = src[key];
+                    }
+                }
+            }
+        }
+        src = arrayShift.call(arguments);
+    }
+
+    return dest;
+}
+
+function joinPath () {
+  return [].reduce.call(arguments, function (prev, path, index, list) {
+
+    path = index ? path.replace(/^\//, '') : path;
+    path = ( index === (list.length - 1) ) ? path : path.replace(/\/$/, '');
+
+    return prev + ( index ? '/' : '' ) + path;
+
+  }, '');
+}
+
+function writeIframe (iframe, content) {
+  var iframeDoc = iframe.contentWindow.document;
+  iframeDoc.charset = 'UTF-8';
+  iframeDoc.open();
+  iframeDoc.write(content);
+  iframeDoc.close();
+}
+
+function getIFrame (iframeStyles) {
+  var iframe = document.createElement('iframe');
+  extend(iframe.style, iframeStyles || {});
+
+  iframe.frameBorder = '0';
+  return iframe;
+}
+
+function template (name, data){
+  return template.cache[name](data || {});
+}
+
+template.cache = {};
+
+template.compile = function (tmpl) {
+  // John Resig micro-template
+  return new Function('obj', // jshint ignore:line
+    'var p=[],print=function(){p.push.apply(p,arguments);};' +
+
+    // Introduce the data as local variables using with(){}
+    'with(obj){p.push(\'' +
+
+    // Convert the template into pure JavaScript
+    tmpl.trim()
+      .replace(/[\r\t\n]/g, ' ')
+      .split('<%').join('\t')
+      .replace(/((^|%>)[^\t]*)'/g, '$1\r')
+      .replace(/\t=(.*?)%>/g, '\',$1,\'')
+      .split('\t').join('\');')
+      .split('%>').join('p.push(\'')
+      .split('\r').join('\\\'') + '\');}return p.join(\'\');');
+};
+
+template.put = function (name, tmpl) {
+  template.cache[name] = template.compile(tmpl);
+};
+
+template.lookup = function () {
+  [].forEach.call(document.querySelectorAll('script[type="application/x-template"][data-template]'), function (tmpl) {
+    template.put(tmpl.getAttribute('data-template'), tmpl.text);
+  });
+};
+
+
+function findBubbleClose (str) {
+  var level = 0;
+
+  for( var i = 0, len = str.length; i < len ; i++ ) {
+    if( str[i] === '(' ) {
+      level++;
+    } else if( str[i] === ')' ) {
+      if( level === 0 ) {
+        return i;
+      } else {
+        level--;
+      }
+    }
+  }
+
+  return -1;
+}
+
+function hasSelector (selector, rootElement) {
+  var splitHas = selector.split(':has(');
+
+  return splitHas.reduce(function (matches, partial) {
+
+    var closePosition = findBubbleClose(partial),
+        hasFilter = partial.substr(0, closePosition),
+        partialQuery = partial.substr(closePosition + 1).trim();
+
+    if( closePosition === -1 ) {
+      throw new Error('malformed selector');
+    }
+
+    matches = matches.filter(function (element) {
+      return element.querySelector(hasFilter);
+    });
+
+    if( partialQuery ) {
+      var submatches = [];
+
+      matches.forEach(function (element) {
+        [].push.apply(submatches, element.querySelectorAll(partialQuery) );
+      });
+
+      return submatches;
+    }
+
+    return matches;
+
+  }, [].slice.call( (rootElement || document).querySelectorAll( splitHas.shift() ) ) );
+}
+
+function querySelector (selector, rootElement) {
+  // 'tr:has(> .row) div:has(span) img'.split(':has(');
+  if( !selector ) {
+    return [];
+  }
+
+  if( !/\:has\(/.test(selector) ) {
+    return [].slice.call( (rootElement || document).querySelectorAll( selector ) );
+  }
+
+  return hasSelector(selector);
+}
+
+function cssQuery (_selector, rootElement) {
+  var selectors = _selector.split(/\s*,\s*/);
+
+  return selectors.reduce(function (list, selector) {
+    return list.concat( querySelector(selector, rootElement) );
+  }, []);
+}
+
+function getAmount (amount) {
+  var prefix = '';
+
+  if( amount < 0 ) {
+    prefix = '-';
+    amount = 0 - amount;
+  }
+
+  if( !amount ) {
+    return '0,00';
+  } else if( amount < 10 ) {
+    return '0,0' + amount;
+  } else if( amount < 100 ) {
+    return '0,' + amount;
+  }
+  return prefix + ('' + amount).replace(/..$/, ',$&');
+}
+
+module.exports = {
+  isObject: _isObject,
+  isFunction: _isFunction,
+  isString: _isString,
+  isNumber: _isNumber,
+  isArray: _isArray,
+  isDate: _isDate,
+  isRegExp: _isRegExp,
+  isElement: _isElement,
+  listen: listen,
+  once: once,
+  ready: docReady,
+  replaceKeys: replaceKeys,
+  merge: merge,
+  extend: extend,
+  joinPath: joinPath,
+  writeIframe: writeIframe,
+  getIFrame: getIFrame,
+  template: template,
+  cssQuery: cssQuery,
+  getAmount: getAmount
+};
+
+},{}],3:[function(require,module,exports){
+// factory http
+
+function headerToTitleSlug(text) {
+  var key = text[0].toUpperCase() + text.substr(1);
+  return key.replace(/([a-z])([A-Z])/, function (match, lower, upper) {
+      return lower + '-' + upper;
+  });
+}
+
+function headerToCamelCase(text) {
+  var key = text[0].toLowerCase() + text.substr(1);
+  return key.replace(/([a-z])-([A-Z])/, function (match, lower, upper) {
+    return lower + upper;
+  });
+}
+
+var RE_contentType = /([^\/]+)\/([^+]+\+)?(.*)/;
+function parseContentType(contentType, text, xml) {
+  var matches = contentType && contentType.match(RE_contentType);
+  return matches && ( matches[3] === 'json' ? JSON.parse(text) : ( matches[3] === 'xml' ? xml : text ) );
+}
+
+function http (url, options) {
+  options = options || {};
+  options.headers = options.headers || {};
+  options.url = url;
+
+  var request = null,
+      on = { resolve: [], reject: [] };
+
+  try { // Firefox, Opera 8.0+, Safari
+      request = new XMLHttpRequest();
+  } catch (e) { // Internet Explorer
+      try { request = new ActiveXObject('Msxml2.XMLHTTP'); }  // jshint ignore:line
+      catch (er) { request = new ActiveXObject('Microsoft.XMLHTTP'); }  // jshint ignore:line
+  }
+  if( request === null ) { throw 'Browser does not support HTTP Request'; }
+
+  if( options.params ) {
+    var i = 0;
+    for( var param in options.params ) {
+      url += ( i++ ? '&' : ( /\?/.test(url) ? '&' : '?' ) ) + param + '=' + encodeURIComponent(options.params[param]);
+    }
+  }
+
+  request.open( ( options.method || 'get').toUpperCase(), url );
+
+  if( options.withCredentials ) {
+    request.withCredentials = true;
+  }
+
+  for( var key in options.headers ) {
+      request.setRequestHeader( headerToTitleSlug(key), options.headers[key] );
+  }
+
+  request.resolve = function ( response ) {
+    on.resolve.forEach(function (handler) {
+      handler(response);
+    });
+  };
+  request.reject = function ( response ) {
+    on.reject.forEach(function (handler) {
+      handler(response);
+    });
+  };
+
+  var headersCache;
+  request.getHeaders = function () {
+    if( !headersCache ) {
+      headersCache = {};
+      request.getAllResponseHeaders().replace(/\s*([^\:]+)\s*\:\s*([^\;\n]+)/g, function (match, key, value) {
+          headersCache[headerToCamelCase(key)] = value.trim();
+      });
+    }
+    return headersCache;
+  };
+
+  request.onreadystatechange = function(){
+    if( request.readyState === 'complete' || request.readyState === 4 ) {
+      var response = {
+        data: parseContentType(request.getResponseHeader('content-type'), request.responseText, request.responseXML),
+        status: request.status,
+        headers: request.getHeaders,
+        xhr: request
+      };
+      if( request.status >= 200 && request.status < 300 ) {
+        request.resolve( response );
+      } else {
+        request.reject( response );
+      }
+    }
+  };
+
+  request.options = options;
+
+  if( options.contentType ) {
+    request.setRequestHeader( 'Content-Type', options.contentType );
+
+    if( options.contentType === 'application/json' && typeof options.data !== 'string' ) {
+      options.data = JSON.stringify(options.data);
+    }
+
+  } else {
+    if( typeof options.data === 'string' ) {
+      options.contentType = 'text/html';
+    } else {
+      options.contentType = 'application/json';
+      options.data = JSON.stringify(options.data);
+    }
+  }
+
+  request.send( options.data );
+
+  return {
+    then: function (onResolve, onReject) {
+      if( onResolve instanceof Function ) {
+        on.resolve.push(onResolve);
+      }
+      if( onReject instanceof Function ) {
+        on.reject.push(onReject);
+      }
+    },
+    error: function (onReject) {
+      if( onReject instanceof Function ) {
+        on.reject.push(onReject);
+      }
+    }
+  };
+}
+
+http.noCache = function (url, options) {
+  url += ( /\?/.test(url) ? '&' : '?' ) + 't=' + new Date().getTime();
+  return http(url, options);
+};
+
+http.plainResponse = function (response) {
+  return {
+    data: response.data,
+    status: response.status,
+    headers: response.headers()
+  };
+};
+
+module.exports = http;
+
+},{}],4:[function(require,module,exports){
+'use strict';
+
+module.exports = function (_) {
+  var suscriptors = [],
+      running = false;
+
+  function initLiveDOM () {
+
+    _.ready(function () {
+      document.body.addEventListener('DOMSubtreeModified', function(event){
+        // console.debug( 'DOM Changed at ', new Date(), event.target );
+        for( var i = 0, n = suscriptors.length; i < n ; i++ ) {
+          suscriptors[i](event.target);
+        }
+      }, false);
+    });
+
+  }
+
+  return {
+    subscribe: function (handler) {
+      if( !running ) {
+        initLiveDOM(true);
+        running = true;
+      }
+      if( handler instanceof Function ) {
+        suscriptors.push(handler);
+      }
+    }
+  };
+
+};
+
+},{}],5:[function(require,module,exports){
+
+module.exports = function (_) {
+
+  var messageTarget = {};
+
+  _.listen(window, 'message', function (e) {
+    var message = e.data,
+        listener = messageTarget[message.aplazame];
+
+    if( !e.used && listener ) {
+      e.used = true;
+      listener(e, message);
+    }
+  });
+
+  return function (target, handler) {
+    if( _.isString(target) && _.isFunction(handler) ) {
+      messageTarget[target] = handler;
+    }
+  };
+
+};
+
+},{}],6:[function(require,module,exports){
+// 'use strict';
+
+var _ = require('./basic-tools');
+
+_.extend(_, {
+  liveDOM: require('./live-dom')(_),
+  http: require('./http'),
+  elementData: document.createElement('div').dataset ? function (el, key, value) {
+    if( value !== undefined ) {
+      el.dataset[key] = value;
+    }
+    return el.dataset[key];
+  } : function (el, key, value) {
+    if( value !== undefined ) {
+      el.setAttribute('data-' + key, value);
+    }
+    return el.getAttribute('data-' + key);
+  },
+  onMessage: require('./message-listener')(_)
+});
+
+module.exports = _;
+
+},{"./basic-tools":2,"./http":3,"./live-dom":4,"./message-listener":5}]},{},[1]);

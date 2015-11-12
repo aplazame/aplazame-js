4511f10e8b1a677a468b5c1c8dda226b330ca61b
Jesús Germade <jesus@germade.es>
2015-11-12 16:14:35 +0100

    http service response includes config data (as angular http does)

--- src/apps/http-service.js
+++ src/apps/http-service.js
@@ -4,28 +4,28 @@ var _ = require('../tools/tools');
 
 _.onMessage('http', function (e, message) {
 
+  function processResponse(result) {
+
+    return function (response) {
+      
+      var responsep = _.http.plainResponse(response);
+      responsep.config = message;
+
+      e.source.postMessage({
+        aplazame: 'http',
+        event: 'response',
+        result: result,
+        response: responsep
+      }, '*');
+    };
+  }
+
   _.http( message.url, {
     method: message.method,
     contentType: message.contentType,
     data: message.data,
     params: message.params
-  } ).then(function (response) {
-    e.source.postMessage({
-      aplazame: 'http',
-      event: 'response',
-      result: 'success',
-      response: _.http.plainResponse(response),
-      srcMessage: message
-    }, '*');
-  }, function (response) {
-    e.source.postMessage({
-      aplazame: 'http',
-      event: 'response',
-      result: 'error',
-      response: _.http.plainResponse(response),
-      srcMessage: message
-    }, '*');
-  });
+  } ).then(processResponse('success'), processResponse('error'));
 
 });
 

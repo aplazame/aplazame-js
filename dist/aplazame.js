46f568f6396b2b17a2a8a2898c0fbab49876376c
Jesús Germade <jesus@germade.es>
2015-11-12 15:23:23 +0100

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.92';
+module.exports = '0.0.93';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -336,14 +336,16 @@ _.onMessage('http', function (e, message) {
       aplazame: 'http',
       event: 'response',
       result: 'success',
-      response: _.http.plainResponse(response)
+      response: _.http.plainResponse(response),
+      srcMessage: message
     }, '*');
   }, function (response) {
     e.source.postMessage({
       aplazame: 'http',
       event: 'response',
       result: 'error',
-      response: _.http.plainResponse(response)
+      response: _.http.plainResponse(response),
+      srcMessage: message
     }, '*');
   });
 });

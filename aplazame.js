1df54f2119763fa2cc539ed5a7cc99e60ddb2c92
Jesús Germade <jesus@germade.es>
2015-12-03 22:48:25 +0100

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.110';
+module.exports = '0.0.111';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -731,7 +731,7 @@ module.exports = function (aplazame) {
     }
 
     var simulators = element.querySelectorAll('[data-aplazame-simulator]'),
-        isMobile = window.matchMedia('( max-width: 768px )');
+        isMobile = window.matchMedia('( max-width: 767px )');
 
     if (simulators.length) {
 

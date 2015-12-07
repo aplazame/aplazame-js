779b4c76419f4294b7c518b49a159c2f1defde4f
Jesús Germade <jesus@germade.es>
2015-12-07 18:31:58 +0100

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.117';
+module.exports = '0.0.118';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -916,10 +916,12 @@ module.exports = function (aplazame) {
             if (choicesCache[currentAmount]) {
               updateWidgetChoices(choicesCache[currentAmount]);
             } else {
-              iframe.contentWindow.postMessage({
-                aplazame: 'simulator',
-                event: 'loading'
-              }, '*');
+              if (iframe) {
+                iframe.contentWindow.postMessage({
+                  aplazame: 'simulator',
+                  event: 'loading'
+                }, '*');
+              }
               aplazame.simulator(currentAmount, function (_choices) {
                 choices = _choices;
                 choicesCache[currentAmount] = _choices;

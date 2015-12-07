a23f31199f5500e9eaa749bf49e1a871ebd7e367
Jesús Germade <jesus@germade.es>
2015-12-07 19:29:02 +0100

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.122';
+module.exports = '0.0.123';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -1342,6 +1342,9 @@ if (!Element.prototype.addEventListener) {
   if (Element.prototype.attachEvent) {
     Element.prototype.addEventListener = function (eventName, listener) {
       return Element.prototype.attachEvent('on' + eventName, listener);
+    };
+    Element.prototype.removeEventListener = function (eventName, listener) {
+      return Element.prototype.detachEvent('on' + eventName, listener);
     };
   } else {
     throw 'Browser not compatible with element events';
--- public/simulator/demo-simulator.js
+++ public/simulator/demo-simulator.js
@@ -444,6 +444,9 @@ if (!Element.prototype.addEventListener) {
     Element.prototype.addEventListener = function (eventName, listener) {
       return Element.prototype.attachEvent('on' + eventName, listener);
     };
+    Element.prototype.removeEventListener = function (eventName, listener) {
+      return Element.prototype.detachEvent('on' + eventName, listener);
+    };
   } else {
     throw 'Browser not compatible with element events';
   }

eb2183dd8f25b69344d74e72e30e5ac2bd833eaf
Jesús Germade <jesus@germade.es>
2015-12-09 15:27:32 +0100

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.124';
+module.exports = '0.0.125';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -776,19 +776,26 @@ module.exports = function (aplazame) {
     var priceSelector = widgetElement.getAttribute('data-price'),
         qtySelector = widgetElement.getAttribute('data-qty');
 
-    if (!priceSelector) {
+    if (priceSelector) {
       try {
-        priceSelector = cmsPriceSelector.find(matchSelector);
+        document.querySelector(priceSelector);
       } catch (err) {
+        priceSelector = null;
         console.warn(err.message);
       }
-
-      if (priceSelector) {
+      if (qtySelector) {
         try {
-          qtySelector = cmsQtySelector.find(matchSelector);
+          document.querySelector(qtySelector);
         } catch (err) {
+          qtySelector = null;
           console.warn(err.message);
         }
+      }
+    } else {
+      priceSelector = cmsPriceSelector.find(matchSelector);
+
+      if (priceSelector) {
+        qtySelector = cmsQtySelector.find(matchSelector);
       }
     }
 

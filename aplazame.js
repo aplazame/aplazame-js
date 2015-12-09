611b27e5d93c19ecab93e1dd15bd63619060d675
Jesús Germade <jesus@germade.es>
2015-12-09 22:51:13 +0100

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.128';
+module.exports = '0.0.129';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -965,8 +965,6 @@ module.exports = function (aplazame) {
           setInterval(function () {
             var amount = getAmount(),
                 qty = getAmount.qtySelector ? getQty(getAmount.qtySelector) : 1;
-
-            console.log('amount', amount);
 
             if (amount && _.isNumber(amount) && amount !== currentAmount || qty !== previousQty) {
               currentAmount = amount;

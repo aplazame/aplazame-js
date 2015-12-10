45ff9c1741b68215234d1d3e8c9b32c0c36c33be
Jesús Germade <jesus@germade.es>
2015-12-10 13:32:51 +0100

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.132';
+module.exports = '0.0.133';
 
 },{}],2:[function(require,module,exports){
 (function (global){
@@ -778,7 +778,7 @@ module.exports = function (aplazame) {
     var price = element.textContent.replace(/[^0-9,.]/g, ''),
         addDot;
 
-    if (/\.,/.test(price)) {
+    if (/[.,]/.test(price)) {
       return price;
     }
 

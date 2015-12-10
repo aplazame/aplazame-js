f1ab4f0547a183f7cd76314ee70348c01ba68fa3
Jesús Germade <jesus@germade.es>
2015-12-10 18:50:49 +0100

    updating built versions

--- public/simulator/demo-simulator.js
+++ public/simulator/demo-simulator.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.137';
+module.exports = '0.0.138';
 
 },{}],2:[function(require,module,exports){
 /* globals aplazame */
@@ -558,7 +558,7 @@ function http(url, config) {
       request.withCredentials = true;
     }
 
-    request.setRequestHeader('X-AJS-Version', apzVersion);
+    // request.setRequestHeader( 'X-AJS-Version', apzVersion );
     for (var key in config.headers) {
       request.setRequestHeader(headerToTitleSlug(key), config.headers[key]);
     }

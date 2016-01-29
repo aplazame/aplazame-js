2ae566234f55b1c37bb3fb25dda251e0bc2f607f
Jesús Germade <jesus@germade.es>
2016-01-29 16:15:00 +0100

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.162';
+module.exports = '0.0.163';
 
 },{}],2:[function(require,module,exports){
 
@@ -1062,7 +1062,7 @@ module.exports = function (aplazame) {
         _.elementData(simulator, 'checked', true);
 
         var getAmount = amountGetter(simulator);
-        currentAmount = Number(simulator.getAttribute('data-amount')) || getAmount();
+        currentAmount = currentAmount || Number(simulator.getAttribute('data-amount')) || getAmount();
 
         var simulatorParams = {
           simulator: '[data-aplazame-simulator]',

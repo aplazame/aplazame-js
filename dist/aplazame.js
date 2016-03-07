0ff996fb8905bd7e8b0ba4079607a0f2012fdc9d
Jesús Germade <jesus@germade.es>
2016-03-07 23:23:44 +0100

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.175';
+module.exports = '0.0.176';
 
 },{}],2:[function(require,module,exports){
 
@@ -901,6 +901,7 @@ module.exports = function (aplazame) {
   // }
 
   function parsePrice(price) {
+    var _price = price;
     price = price.match(/[\d,.]+/);
     price = price && price[0] || '';
     var priceParts = ('' + price).replace(/[^0-9.,]/g, '').split(/[,.]/),
@@ -920,6 +921,7 @@ module.exports = function (aplazame) {
       amount += Number(piece);
       piece = priceParts.shift();
     }
+    console.log('parsePrice', _price, amount);
     return amount;
   }
 

955ec5f394c5c205661bed3981250bef05cc39ce
Jesús Germade <jesus@germade.es>
2016-03-12 13:31:56 +0100

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.181';
+module.exports = '0.0.182';
 
 },{}],2:[function(require,module,exports){
 
@@ -350,26 +350,21 @@ function checkout(options) {
 
   cssOverlay.hack(true);
 
-  if (checkout.animatedLogo) {
-    cssBlur.hack(true);
+  cssBlur.hack(true);
 
-    setTimeout(function () {
-      _.addClass(document.body, 'aplazame-blur');
-    }, 0);
+  setTimeout(function () {
+    _.addClass(document.body, 'aplazame-blur');
+  }, 0);
 
-    tmpOverlay.innerHTML = '<div class="aplazame-logo-wrapper"><div class="logo-aplazame" style="width: 150px; height: 150px;">' + '<svg class="line-short" version="1.1" viewBox="0 0 100 100">' + '<path  d="M36.788,81.008,50,50" stroke-linecap="round" stroke-width="6" fill="none"/>' + '</svg>' + '<svg class="smile" version="1.1" viewBox="0 0 100 100">' + '<g stroke-linecap="round" fill="none" transform="matrix(0.78036633,0,0,0.78036633,10.526512,18.003998)">' + '<path class="smile-outline" stroke-width="12" d="M75.242,57.51c-5.435,7.839-14.498,12.972-24.761,12.972-10.262,0-19.325-5.132-24.758-12.972"/>' + '<path class="smile-line" stroke-width="7.5" d="M75.242,57.51c-5.435,7.839-14.498,12.972-24.761,12.972-10.262,0-19.325-5.132-24.758-12.972"/>' + '</g>' + '</svg>' + '<svg class="line-large" version="1.1" viewBox="0 0 100 100">' + '<path stroke-linejoin="round" d="M50,50,66.687,92.266" stroke-linecap="round" stroke-miterlimit="4" stroke-dasharray="none" stroke-width="6" fill="none"/>' + '</svg>' + '</div>' + '<div class="aplazame-checkout-loading-text">cargando pasarela de pago...</div></div>';
-  }
+  tmpOverlay.innerHTML = '<div class="aplazame-logo-wrapper"><div class="logo-aplazame" style="width: 150px; height: 150px;">' + '<svg class="line-short" version="1.1" viewBox="0 0 100 100">' + '<path  d="M36.788,81.008,50,50" stroke-linecap="round" stroke-width="6" fill="none"/>' + '</svg>' + '<svg class="smile" version="1.1" viewBox="0 0 100 100">' + '<g stroke-linecap="round" fill="none" transform="matrix(0.78036633,0,0,0.78036633,10.526512,18.003998)">' + '<path class="smile-outline" stroke-width="12" d="M75.242,57.51c-5.435,7.839-14.498,12.972-24.761,12.972-10.262,0-19.325-5.132-24.758-12.972"/>' + '<path class="smile-line" stroke-width="7.5" d="M75.242,57.51c-5.435,7.839-14.498,12.972-24.761,12.972-10.262,0-19.325-5.132-24.758-12.972"/>' + '</g>' + '</svg>' + '<svg class="line-large" version="1.1" viewBox="0 0 100 100">' + '<path stroke-linejoin="round" d="M50,50,66.687,92.266" stroke-linecap="round" stroke-miterlimit="4" stroke-dasharray="none" stroke-width="6" fill="none"/>' + '</svg>' + '</div>' + '<div class="aplazame-checkout-loading-text">cargando pasarela de pago...</div></div>';
 
   document.body.appendChild(tmpOverlay);
+  var loadingText = tmpOverlay.querySelector('.aplazame-checkout-loading-text'),
+      logo = tmpOverlay.querySelector('.logo-aplazame');
 
-  if (checkout.animatedLogo) {
-    var loadingText = tmpOverlay.querySelector('.aplazame-checkout-loading-text'),
-        logo = tmpOverlay.querySelector('.logo-aplazame');
-
-    setTimeout(function () {
-      logo.className += ' animate';
-    }, 200);
-  }
+  setTimeout(function () {
+    logo.className += ' animate';
+  }, 200);
 
   options.api = api;
 
@@ -468,7 +463,7 @@ function checkout(options) {
           // confirmation_url
           break;
         case 'close':
-          if (iframe && message.close) {
+          if (iframe) {
             document.body.removeChild(iframe);
             cssModal.hack(false);
             iframe = null;
@@ -488,31 +483,31 @@ function checkout(options) {
           break;
       }
 
-      if (message.require === 'merchant') {
-        cssModal.hack(true);
-        _.addClass(document.body, 'aplazame-blur');
-        cssOverlay.hack(false);
-        document.body.removeChild(tmpOverlay);
-        e.source.postMessage({
-          checkout: options
-        }, '*');
-      } else if (iframe && message.close) {
-        document.body.removeChild(iframe);
-        cssModal.hack(false);
-        iframe = null;
-
-        switch (message.close) {
-          case 'dismiss':
-            location.replace(options.merchant.checkout_url || '/');
-            break;
-          case 'success':
-            location.replace(options.merchant.success_url);
-            break;
-          case 'cancel':
-            location.replace(options.merchant.cancel_url);
-            break;
-        }
-      }
+      // if( message.require === 'merchant' ) {
+      //   cssModal.hack(true);
+      //   _.addClass(document.body, 'aplazame-blur');
+      //   cssOverlay.hack(false);
+      //   document.body.removeChild(tmpOverlay);
+      //   e.source.postMessage({
+      //     checkout: options
+      //   }, '*');
+      // } else if( iframe && message.close ) {
+      //   document.body.removeChild(iframe);
+      //   cssModal.hack(false);
+      //   iframe = null;
+      //
+      //   switch( message.close ) {
+      //     case 'dismiss':
+      //       location.replace(options.merchant.checkout_url || '/');
+      //       break;
+      //     case 'success':
+      //       location.replace(options.merchant.success_url);
+      //       break;
+      //     case 'cancel':
+      //       location.replace(options.merchant.cancel_url);
+      //       break;
+      //   }
+      // }
     });
   }, function () {
     throw new Error('can not connect to ' + baseUrl);

8f7c3e6fc39132e26f679b361aa2a938567d5fad
Jesús Germade <jesus@germade.es>
2016-03-14 12:17:47 +0100

    styled pay with aplazame

--- src/apps/checkout.js
+++ src/apps/checkout.js
@@ -50,7 +50,7 @@ function checkout (options) {
 
   setTimeout(function () {
     tmpOverlay.querySelector('.logo-aplazame').className += ' animate';
-  }, 100);
+  }, 200);
 
   options.api = api;
 

66fa09b0649b4b6b15c8a0db0f408931feb7348d
Jesús Germade <jesus@germade.es>
2016-03-15 11:02:08 +0100

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.191';
+module.exports = '0.0.192';
 
 },{}],2:[function(require,module,exports){
 
@@ -426,10 +426,10 @@ function checkout(options) {
           }, '*');
           break;
         case 'show-iframe':
-          // _.removeClass(iframe, 'hide');
-          // cssModal.hack(true);
-          // cssOverlay.hack(false);
-          // document.body.removeChild(tmpOverlay);
+          _.removeClass(iframe, 'hide');
+          cssModal.hack(true);
+          cssOverlay.hack(false);
+          document.body.removeChild(tmpOverlay);
           break;
         case 'loading-text':
           loadingText.textContent = message.text;

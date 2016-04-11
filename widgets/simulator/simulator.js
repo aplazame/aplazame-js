b9b3c8e49f8636f84167504553db4fa47fe32402
Jesús Germade <jesus@germade.es>
2016-04-11 13:04:11 +0200

    fixed widget amount update

--- widgets/simulator/simulator.js
+++ widgets/simulator/simulator.js
@@ -81,7 +81,15 @@ var main = document.getElementById('main'), currentMessage,
       choices: function (message) {
         currentMessage = message;
         currentMessage.$$choice = currentMessage.choices.reduce(maxInstalments, null);
-        renderWidget();
+
+        var price = document.getElementById('.aplazame-widget-button .amount .price');
+
+        if( price ) {
+          price.textContent = _.getAmount(currentMessage.$$choice.amount);
+        } else {
+          renderWidget();
+        }
+
       },
       loading: function (message) {
         _.addClass(main, 'loading');

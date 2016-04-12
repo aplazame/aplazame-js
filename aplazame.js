0d0fc1b04aacc508053460a34d17ac044f382d8d
Jesús Germade <jesus@germade.es>
2016-04-12 18:24:29 +0200

    updating built versions

--- aplazame.js
+++ aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.229';
+module.exports = '0.0.230';
 
 },{}],2:[function(require,module,exports){
 module.exports = '@-webkit-keyframes aplazame-blur{0%{-webkit-filter:blur(0);filter:blur(0);}to{-webkit-filter:blur(3px);filter:blur(3px)}}@keyframes aplazame-blur{0%{-webkit-filter:blur(0);filter:blur(0)}to{-webkit-filter:blur(3px);filter:blur(3px)}}@media (min-width:601px){body.aplazame-blur>:not(.aplazame-modal):not(.aplazame-overlay){-webkit-filter:blur(3px);filter:blur(3px);-webkit-animation-duration:.4s;animation-duration:.4s;-webkit-animation-name:aplazame-blur;animation-name:aplazame-blur}body.aplazame-unblur>:not(.aplazame-modal):not(.aplazame-overlay){-webkit-filter:blur(0);filter:blur(0);-webkit-animation-duration:.4s;animation-duration:.4s;-webkit-animation-name:aplazame-blur;animation-name:aplazame-blur;-webkit-animation-direction:reverse;animation-direction:reverse}}';
@@ -1015,6 +1015,13 @@ global.aplazame.info = function () {
   };
 };
 
+global.aplazame.log = function () {
+  require('./tools/log').history.forEach(function (l) {
+    console.log(l.time);
+    console.log.apply(console, l.args);
+  });
+};
+
 global.$q = require('q-promise');
 global.$http = require('http-browser');
 
@@ -1856,7 +1863,7 @@ module.exports = function (aplazame) {
             var matched = el.textContent.match(/[\d,.]+/);
 
             if (matched) {
-              amount += matched[0];
+              amount += (amount ? '.' : '') + matched[0];
             }
           });
         } else {

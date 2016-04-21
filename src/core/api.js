e1e1640fde0bc6d696988463d3b89d009bb15188
Jesús Germade <jesus@germade.es>
2016-04-21 16:08:05 +0200

    widget raw opens info modal

--- src/core/api.js
+++ src/core/api.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   host: ( /^js\.aplazame\.[a-z]+$/.test(location.host) || location.host === 'demo.debug.aplazame.com' ) ? 'https://api.dev.aplazame.com/' : 'https://api.aplazame.com/',
+  // host: 'https://api.aplazame.com/',
   baseUrl: 'https://aplazame.com/static/',
   version: 1,
   checkoutVersion: 1,

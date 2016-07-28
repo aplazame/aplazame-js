0d175077435635f79ef01df5d79fe97f0d680645
Jesús Germade <jesus@germade.es>
2016-07-28 21:42:50 +0200

    ignoring drone.sec variable

--- src/core/api.js
+++ src/core/api.js
@@ -1,7 +1,13 @@
 'use strict';
 
+var apiDevHosts = [
+  'demo-dev.aplazame.com',
+  'magento.aplazame.com',
+  'prestashop.aplazame.com'
+];
+
 module.exports = {
-  host: ( /^js\.aplazame\.[a-z]+$/.test(location.host) || location.host === 'demo-dev.aplazame.com' ) ? 'https://api-dev.aplazame.com/' : 'https://api.aplazame.com/',
+  host: ( /^js\.aplazame\.[a-z]+$/.test(location.hostname) || apiDevHosts.indexOf(location.hostname) >= 0 ) ? 'https://api-dev.aplazame.com/' : 'https://api.aplazame.com/',
   // host: 'https://api.aplazame.com/',
   baseUrl: 'https://aplazame.com/static/',
   version: 1,

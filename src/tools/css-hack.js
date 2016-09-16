0d10a17b780c45e2ee1d3641a5d4f55c4e180157
Jesús Germade <jesus@germade.es>
2016-09-16 15:57:17 +0200

    enhancing checkout modal styles

--- src/tools/css-hack.js
+++ src/tools/css-hack.js
@@ -1,41 +1,50 @@
 
 
-var cssHack = (function () {
-  var cache = {},
-      hacks = {
-        overlay: require('../../.tmp/css-hacks/overlay'),
-        blur: require('../../.tmp/css-hacks/blur'),
-        logo: require('../../.tmp/css-hacks/logo'),
-        modal: require('../../.tmp/css-hacks/modal')
-      };
+var importantCSS = function (css) {
+      return css.replace(/;/g, ' !important;').replace(/:([^:;]+?)}/g, function (matched, value) {
+        return ':' + value + ' !important;}';
+      }).replace(/(\d+%|from|to){([^}]+)/g, function (matched, pos, content) {
+        return pos + '{' + content.replace(/ !important;/g, ';');
+      });
+    },
+    cssHack = (function () {
+      var cache = {},
+          hacks = {
+            overlay: importantCSS(require('../../.tmp/css-hacks/overlay')),
+            blur: importantCSS(require('../../.tmp/css-hacks/blur')),
+            logo: importantCSS(require('../../.tmp/css-hacks/logo')),
+            modal: importantCSS(require('../../.tmp/css-hacks/modal'))
+          };
 
-  return function hack (hackName) {
-    if( !cache[hackName] ) {
-      var style = document.createElement('style');
-      style.setAttribute('rel', 'stylesheet');
-      style.setAttribute('type', 'text/css');
-      style.textContent = hacks[hackName];
+      console.log('cssHacks', hacks);
 
-      var enabled = false;
+      return function hack (hackName) {
+        if( !cache[hackName] ) {
+          var style = document.createElement('style');
+          style.setAttribute('rel', 'stylesheet');
+          style.setAttribute('type', 'text/css');
+          style.textContent = hacks[hackName];
 
-      style.hack = function (enable) {
-        enable = enable === undefined || enable;
+          var enabled = false;
 
-        if( enable ) {
-          if( enabled ) { return; }
-          enabled = true;
-          document.head.appendChild(style);
-        } else {
-          if( !enabled ) { return; }
-          enabled = false;
-          document.head.removeChild(style);
+          style.hack = function (enable) {
+            enable = enable === undefined || enable;
+
+            if( enable ) {
+              if( enabled ) { return; }
+              enabled = true;
+              document.head.appendChild(style);
+            } else {
+              if( !enabled ) { return; }
+              enabled = false;
+              document.head.removeChild(style);
+            }
+          };
+
+          cache[hackName] = style;
         }
+        return cache[hackName];
       };
-
-      cache[hackName] = style;
-    }
-    return cache[hackName];
-  };
-})();
+    })();
 
 module.exports = cssHack;

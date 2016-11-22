fdbe66a39570946a21d64bfb28e33ec155f37d6e
Maks3w <github.maks3w@virtualplanets.net>
2016-11-22 11:26:18 +0100

    [checkout] normalizer

---
+++ src/apps/checkout-normalizer.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function checkoutNormalizer(checkout, location, api) {
+  checkout.origin = {
+    host: location.host,
+    href: location.href,
+    origin: location.origin,
+    protocol: location.protocol
+  };
+
+  checkout.api = api;
+
+  if( !checkout.merchant ) {
+    throw new Error('missing merchant parameters');
+  }
+
+  if( !checkout.merchant.public_api_key && !api.publicKey ) {
+    throw new Error('missing public key');
+  }
+
+  checkout.merchant.public_api_key = checkout.merchant.public_api_key || api.publicKey;
+  checkout.merchant.sandbox = checkout.merchant.sandbox === undefined ? api.sandbox : checkout.merchant.sandbox;
+
+  if (!checkout.merchant.onSuccess && !checkout.merchant.success_url) {
+    throw new Error('success_url missing');
+  }
+  checkout.merchant.onSuccess = checkout.merchant.onSuccess || function () {
+    location.replace(checkout.merchant.success_url);
+  };
+
+  if (!checkout.merchant.onError && !checkout.merchant.cancel_url) {
+    throw new Error('cancel_url missing');
+  }
+  checkout.merchant.onError = checkout.merchant.onError || function () {
+    location.replace(checkout.merchant.cancel_url);
+  };
+
+  checkout.merchant.onDismiss = checkout.merchant.onDismiss || function () {
+    location.replace(checkout.merchant.checkout_url || '/');
+  };
+
+  return checkout;
+}
+
+module.exports = checkoutNormalizer;
---
+++ tests/checkout-normalizer-tests.js
@@ -0,0 +1,140 @@
+
+var assert = require('assert'),
+    checkoutNormalizer = require('../src/apps/checkout-normalizer');
+
+describe('amount getter', function () {
+
+  // Store last href so it can be inspected.
+  var lastHref;
+
+  var location = {
+    host: 'example.com',
+    href: 'http://example.com:8080/foo?bas=baz#fooId',
+    origin: 'http://example.com',
+    protocol: 'https:',
+    replace: function( href ) {
+      if (href) {
+        lastHref = href;
+      }
+
+      return lastHref;
+    }
+  };
+
+  var api = {
+    publicKey: 'fooKey',
+    sandbox: true
+  };
+
+  it('throw exception when merchant is not provided', function() {
+    var checkout = {};
+
+    assert.throws(
+      function () {
+        checkoutNormalizer(checkout, location, api);
+      },
+      /missing merchant parameters/
+    );
+  });
+
+  it('throw exception when merchant.success_url is not provided', function() {
+    var checkout = {
+      merchant: {}
+    };
+
+    assert.throws(
+      function () {
+        checkoutNormalizer(checkout, location, api);
+      },
+      /success_url missing/
+    );
+  });
+
+  it('throw exception when merchant.cancel_url is not provided', function() {
+    var checkout = {
+      merchant: {
+        success_url: '/success_url'
+      }
+    };
+
+    assert.throws(
+      function () {
+        checkoutNormalizer(checkout, location, api);
+      },
+      /cancel_url missing/
+    );
+  });
+
+  it('minimum fields are provided', function() {
+    var checkout = {
+      merchant: {
+        success_url: '/success_url',
+        cancel_url: '/cancel_url'
+      }
+    };
+
+    checkout = checkoutNormalizer(checkout, location, api);
+
+    checkout.merchant.onSuccess();
+    assert.equal(location.replace(), '/success_url');
+
+    checkout.merchant.onError();
+    assert.equal(location.replace(), '/cancel_url');
+
+    checkout.merchant.onDismiss();
+    assert.equal(location.replace(), '/');
+
+    delete checkout.merchant.onSuccess;
+    delete checkout.merchant.onError;
+    delete checkout.merchant.onDismiss;
+
+    assert.deepEqual(checkout, {
+      api: {
+        publicKey: 'fooKey',
+        sandbox: true
+      },
+      merchant: {
+        success_url: '/success_url',
+        cancel_url: '/cancel_url',
+        public_api_key: 'fooKey',
+        sandbox: true
+      },
+      origin: {
+        host: 'example.com',
+        href: 'http://example.com:8080/foo?bas=baz#fooId',
+        origin: 'http://example.com',
+        protocol: 'https:'
+      }
+    });
+  });
+
+  it('provided public_api_key is preserved', function() {
+    var checkout = {
+      merchant: {
+        public_api_key: 'customApiKey',
+        success_url: '/success_url',
+        cancel_url: '/cancel_url'
+      }
+    };
+
+    checkout = checkoutNormalizer(checkout, location, api);
+
+    assert.equal(checkout.merchant.public_api_key, 'customApiKey');
+    assert.equal(checkout.api.publicKey, 'fooKey');
+  });
+
+  it('provided sandbox value is preserved', function() {
+    var checkout = {
+      merchant: {
+        sandbox: false,
+        success_url: '/success_url',
+        cancel_url: '/cancel_url'
+      }
+    };
+
+    checkout = checkoutNormalizer(checkout, location, api);
+
+    assert.equal(checkout.merchant.sandbox, false);
+    assert.equal(checkout.api.sandbox, true);
+  });
+});

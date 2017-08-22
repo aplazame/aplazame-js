56229c19f2981f913670b98798821bc27f54c632
Jesús Germade <jesus@germade.es>
2017-08-23 01:22:06 +0200

    cleaning logs and lint

--- src/loaders/data-simulator.js
+++ src/loaders/data-simulator.js
@@ -2,36 +2,9 @@
 
 module.exports = function (aplazame) {
 
-  var _ = aplazame._,
-      // log = require('../tools/log'),
-      // $live = require('../tools/live-dom'),
-      $live = require('live-dom'),
+  var $live = require('live-dom'),
       _amountGetter = require('./data-simulator-amount')(aplazame),
       Widget = require('./data-simulator-widget')(aplazame);
-
-  // var dom_listeners = [],
-  //     _removeListener = function (listener) {
-  //       for( var i = dom_listeners.length - 1 ; i >= 0 ; i-- ) {
-  //         if( listener === dom_listeners[i] ) dom_listeners.splice(i, 1);
-  //       }
-  //     };
-  //     onNodeChanges = window.MutationObserver ? function (onNodeChanged) {
-  //       try{
-  //         new MutationObserver(function(mutations) {
-  //           mutations.forEach( onNodeChanged );
-  //         }).observe(document.body, { childList: true, subtree: true });
-  //       } catch(err) {
-  //         document.body.addEventListener('DOMSubtreeModified', onNodeChanged);
-  //       }
-  //     } : function (onNodeChanged) {
-  //       document.body.addEventListener('DOMSubtreeModified', onNodeChanged);
-  //     };
-  //
-  // onNodeChanges( _.debounce(function () {
-  //   dom_listeners.forEach(function (listener) {
-  //     listener();
-  //   });
-  // }) );
 
   $live('[data-aplazame-simulator]', function (widget_el) {
 

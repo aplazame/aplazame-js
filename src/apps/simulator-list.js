72cf49e62428fa1892a455c5519dfae1c650ee36
Héctor <snake_raiden35@hotmail.com>
2017-09-13 15:41:37 +0200

    Calculate quotas with merchant campaigns and options

--- src/apps/simulator-list.js
+++ src/apps/simulator-list.js
@@ -1,9 +1,98 @@
 'use strict';
 
-var apiHttp = require('../core/api-http'),
-    _ = require('../tools/tools'),
-    simulatorCampaigns = require('./simulator-campaigns'),
-    simulatorOptions = require('./simulator-options'),
-    log = require('../tools/log');
+var simulatorCampaigns = require('./simulator-campaigns'),
+    simulatorOptions = require('./simulator-options');
 
-module.exports = simulatorCampaigns;
+function simulatorList(product){
+
+  var id = product.amount;
+  var price = product.productId;
+  var options = simulatorOptions();
+
+  // En funcion del ID del producto y de las opciones de la tienda obtenemos las campañas que aplican a cada mes
+  var productCampaigns = getCampaignByMonth(id);
+
+  var quotas = [];
+  for (var i = 0; i < 12; i++ ){
+    var poly = new Poly(productCampaigns[i].coeffs);
+    var downpayment;
+    var quota;
+
+    productCampaigns[i].polynom = poly.jsFunction;
+    quota = productCampaigns[i].polynom(i+1) * price;
+
+    if (i === 0){
+      downpayment = options.d_deferred_rate*quota;
+    } else {
+      downpayment = options.m_merchant*quota;
+    }
+
+    var output = {
+      instalment: i+1,
+      downpayment: downpayment,
+      quota: quota
+    };
+    quotas.push(output);
+
+  }
+
+  return quotas;
+
+}
+
+function Poly(coeffs){
+  this.mathFunction = '';
+  this.jsFunction = '';
+
+  for (var i = 0; i < coeffs.length ; i++){
+    var sign = (coeffs[i]>=0)?'+':'';
+    var grade = (coeffs.length-i-1);
+    this.jsFunction += sign + coeffs[i] + '*Math.pow(x,' + grade + ')';
+    this.mathFunction += sign + coeffs[i] + '*x^' + grade;
+  }
+  this.jsFunction = new Function('x','return ' + this.jsFunction);
+}
+
+function getCampaignByMonth(id){
+      var filteredCampaign = [];
+      var merchantInfo = simulatorCampaigns();
+
+      for (var i = 1; i <= 12; i++){
+
+        var bestCampaign = {
+            annual_equivalent: merchantInfo.annual_equivalent,
+            coeffs: merchantInfo.coeffs
+        };
+        // Recuperamos las campañas que aplican al mes i
+        var campaigns = [];
+        if (merchantInfo.campaigns){
+          campaigns = merchantInfo.campaigns.filter(function(campaign){
+            return campaign.num_instalment.indexOf(i) > -1;
+          });
+        }
+
+        if (campaigns.length){
+
+          for (var j = 0, campaignLength = campaigns.length; j < campaignLength; j++){
+
+            var campaign = merchantInfo.campaigns[j];
+            if (campaign.products.indexOf(id) > -1){
+
+              if (bestCampaign.annual_equivalent > campaign.annual_equivalent){
+
+                bestCampaign = campaign;
+              }
+            }
+
+          }
+
+        }
+
+        filteredCampaign.push( bestCampaign );
+      }
+
+      return filteredCampaign;
+    }
+
+
+module.exports = simulatorList;

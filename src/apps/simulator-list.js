5db82ff8e80240c52dd7dec84ab26534eb123e6e
Héctor <snake_raiden35@hotmail.com>
2017-09-13 09:57:56 +0200

    wip

---
+++ src/apps/simulator-list.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var apiHttp = require('../core/api-http'),
+    _ = require('../tools/tools'),
+    simulatorCampaigns = require('./simulator-campaigns'),
+    simulatorOptions = require('./simulator-options'),
+    log = require('../tools/log');
+
+module.exports = simulatorCampaigns;

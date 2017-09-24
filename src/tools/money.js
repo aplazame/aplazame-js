9e1b70e20c29c36e2d3290ea767fe5af8758a611
Maks3w <github.maks3w@virtualplanets.net>
2017-09-24 21:03:07 +0200

    [Money] This model is used for the tuple amount+currency

---
+++ src/tools/money.js
@@ -0,0 +1,12 @@
+"use strict";
+module.exports = function (amount, currency) {
+  var _self = this;
+
+  return {
+    amount: amount,
+    currency: currency,
+    equals: function (money) {
+      return (_self.amount === money.amount ) && (_self.currency === money.currency)
+    }
+  }
+};

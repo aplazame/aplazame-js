2aaf868fff708fc2f5ec867ed191526be264657c
Héctor <snake_raiden35@hotmail.com>
2017-11-15 14:52:29 +0100

    updating built versions

--- dist/aplazame.js
+++ dist/aplazame.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = '0.0.453';
+module.exports = '0.0.454';
 },{}],2:[function(require,module,exports){
 module.exports = '@keyframes aplazame-blur{0%{-webkit-filter:blur(0);filter:blur(0);}to{-webkit-filter:blur(1px);filter:blur(1px)}}body.aplazame-blur>:not(.aplazame-modal):not(.aplazame-overlay){-webkit-filter:blur(1px);filter:blur(1px)}@media (min-width:601px){body.aplazame-blur>:not(.aplazame-modal):not(.aplazame-overlay){animation-duration:.4s;animation-name:aplazame-blur}}body.aplazame-unblur>:not(.aplazame-modal):not(.aplazame-overlay){-webkit-filter:blur(0);filter:blur(0)}@media (min-width:601px){body.aplazame-unblur>:not(.aplazame-modal):not(.aplazame-overlay){animation-duration:.4s;animation-name:aplazame-blur;animation-direction:reverse}}';
 },{}],3:[function(require,module,exports){
@@ -3306,7 +3306,9 @@ module.exports = function (aplazame) {
 
     if( priceSelector ) {
       try{
-        if( !document.querySelector(priceSelector) ) priceSelector = null;
+        priceSelector = priceSelector.split(/ *, */).reduce(function (matched, selector) {
+          return matched || (document.querySelector(selector) && selector) || null;
+        }, null);
       } catch(err) {
         priceSelector = null;
         log('data-price: missing', err.message);
@@ -3626,6 +3628,7 @@ module.exports = function (aplazame) {
           // if( !document.body.contains(widget_el) ) return _removeListener(onDomChanges);
           if( !document.body.contains(widget_el) ) return $live.off(onDomChanges);
 
+          amountGetter = _amountGetter(widget_el);
           var amount = amountGetter();
 
           if( amount && amount !== current_amount ) updateAmount(amount);

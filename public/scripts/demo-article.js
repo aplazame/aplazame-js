77a71fc970230acf42a3bccbf41c154afd65a892
Jesús Germade <jesus@germade.es>
2017-11-24 12:58:21 +0100

    updating built versions

--- public/scripts/demo-article.js
+++ public/scripts/demo-article.js
@@ -83,6 +83,8 @@ aplazame._.ready(function () {
   var http = require('http-rest/browser'),
       checkoutData = http(params['checkout-json']);
 
+  http.usePromise(require('parole'));
+
   function randOrderId () {
     return 'test-' + new Date().getTime();
   }
@@ -139,7 +141,7 @@ aplazame._.ready(function () {
 
 });
 
-},{"http-rest/browser":2}],2:[function(require,module,exports){
+},{"http-rest/browser":2,"parole":3}],2:[function(require,module,exports){
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
@@ -526,5 +528,227 @@ http$1.useRequest(xmlRequest);
 return http$1;
 
 })));
+
+},{}],3:[function(require,module,exports){
+
+(function (root, factory) {
+  if( typeof exports === 'object' && typeof module !== 'undefined' ) {
+    // CommonJS
+    module.exports = factory();
+  } else if( typeof define === 'function' && define.amd ) {
+      // AMD. Register as an anonymous module.
+      define([], factory);
+  } else {
+      // Browser globals
+      root.Parole = factory();
+  }
+})(this, function () {
+
+  function runHandler (fn, deferred, x, fulfilled) {
+    if( typeof fn === 'function' ) {
+      try {
+        deferred.resolve( fn(x) );
+      } catch(reason) {
+        deferred.reject( reason );
+      }
+    } else {
+      deferred[ fulfilled ? 'resolve' : 'reject' ](x);
+    }
+  }
+
+  function resolvePromise (p, x, fulfilled) {
+    if( p.resolved ) {
+      return;
+    }
+    p.resolved = true;
+
+    p.result = x;
+    p.fulfilled = fulfilled || false;
+
+    var queue = p.queue.splice(0);
+    p.queue = null;
+
+    setTimeout(function () {
+      for( var i = 0, n = queue.length ; i < n ; i++ ) {
+        runHandler( queue[i][fulfilled ? 0 : 1], queue[i][2], x, fulfilled );
+      }
+    }, 0);
+  }
+
+  function runThenable (then, p, x) {
+    var executed = false;
+    try {
+      then.call(x, function (value) {
+        if( executed ) return;
+        executed = true;
+        xThen(p, value, true);
+      }, function (reason) {
+        if( executed ) return;
+        executed = true;
+        xThen(p, reason, false);
+      });
+    } catch(err) {
+      if( executed ) return;
+      xThen(p, err, false);
+    }
+  }
+
+  function xThen (p, x, fulfilled) {
+    var then;
+
+    if( x && ( typeof x === 'object' || typeof x === 'function' ) ) {
+      try {
+        then = x.then;
+
+        if( fulfilled && typeof then === 'function' ) {
+          runThenable(then, p, x);
+        } else {
+          resolvePromise(p, x, fulfilled);
+        }
+      } catch (reason) {
+        resolvePromise(p, reason, false);
+      }
+    } else {
+      resolvePromise(p, x, fulfilled);
+    }
+  }
+
+  function resolveProcedure (p, x, fulfilled) {
+    if( p.resolving ) return;
+    p.resolving = true;
+
+    if( x === p.promise ) {
+      fulfilled = false;
+      x = new TypeError('A promise can not be resolved by itself');
+    }
+
+    xThen(p, x, fulfilled);
+  }
+
+  function Parole (resolver) {
+    if( !(this instanceof Parole) ) {
+      return new Parole(resolver);
+    }
+
+    if( typeof resolver !== 'function' ) {
+      throw new TypeError('Promise resolver ' + resolver + ' is not a function');
+    }
+
+    var p = {
+      queue: [],
+      promise: this
+    };
+
+    this.__promise = p;
+
+    try {
+      resolver(function (value) {
+        resolveProcedure(p, value, true);
+      }, function (reason) {
+        resolveProcedure(p, reason, false);
+      });
+    } catch (reason) {
+      resolveProcedure(p, reason, false);
+    }
+
+  }
+
+  Parole.prototype.then = function (onFulfilled, onRejected) {
+    var p = this.__promise,
+        deferred = Parole.defer();
+
+    if( p.queue ) {
+      p.queue.push([onFulfilled, onRejected, deferred]);
+    } else {
+      setTimeout(function () {
+        runHandler( p.fulfilled ? onFulfilled : onRejected, deferred, p.result, p.fulfilled );
+      }, 0);
+    }
+
+    return deferred.promise;
+  };
+
+  Parole.prototype.catch = function (onRejected) {
+    return this.then(null, onRejected);
+  };
+
+  // Promise methods
+
+  function each (iterable, handler) {
+    for( var i = 0, n = iterable.length; i < n ; i++ ) {
+      handler(iterable[i], i);
+    }
+  }
+
+  Parole.defer = function () {
+    var deferred = {};
+    deferred.promise = new Parole(function (resolve, reject) {
+      deferred.resolve = resolve;
+      deferred.reject = reject;
+    });
+    return deferred;
+  };
+
+  Parole.when = function (x) { return ( x && x.then ) ? x : Parole.resolve(x); };
+
+  Parole.resolve = function (value) {
+    return new Parole(function (resolve) {
+      resolve(value);
+    });
+  };
+
+  Parole.reject = function (value) {
+    return new Parole(function (resolve, reject) {
+      reject(value);
+    });
+  };
+
+  Parole.all = function (iterable) {
+    return new Parole(function (resolve, reject) {
+      var pending = iterable.length,
+          results = [];
+      each(iterable, function (_promise, i) {
+
+        ( _promise.then ? _promise : Parole.resolve(_promise) ).then(function (result) {
+          results[i] = result;
+          if( --pending === 0 ) {
+            resolve(results);
+          }
+        }, function (reason) {
+          if( pending !== -1 ) {
+            pending === -1;
+            reject(reason);
+          }
+        });
+      });
+    });
+  };
+
+  Parole.race = function (iterable) {
+    return new Parole(function (resolve, reject) {
+      var done = false;
+
+      each(iterable, function (_promise) {
+        if( done ) {
+          return;
+        }
+        ( _promise.then ? _promise : Parole.resolve(_promise) ).then(function (result) {
+          if( !done ) {
+            done = true;
+            resolve(result);
+          }
+        }, function (reason) {
+          if( !done ) {
+            done = true;
+            reject(reason);
+          }
+        });
+      });
+    });
+  };
+
+  return Parole;
+
+});
 
 },{}]},{},[1]);

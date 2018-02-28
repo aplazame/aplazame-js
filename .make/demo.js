0f6f1aab4b97b4f5702bcaae85f347fc493d658d
Jesús Germade <jesus@germade.es>
2018-02-28 16:49:14 +0100

    Adding thousands separator to demo amounts

--- .make/demo.js
+++ .make/demo.js
@@ -78,14 +78,19 @@ module.exports = function (nitro) {
 
     // console.log('-------------------\nbranch: ' + require('child_process').execSync('git symbolic-ref --short -q HEAD 2>/dev/null') + '\n-------------------');
 
+    function _separateThousands(amount, thousands_separator) {
+      if( amount < 1000 ) return '' + amount;
+      return _separateThousands( parseInt(amount/1000), thousands_separator ) + thousands_separator + amount%1000;
+    }
+
     var pkg = require('../package'),
         dev = target === 'dev',
         branch = ('' + require('child_process').execSync('git symbolic-ref --short -q HEAD 2>/dev/null')).trim(),
         renderIndex = template( file.read('demo/index.html') ),
         checkout = file.readJSON('./demo/checkout-ES.json'),
-        checkoutMX = file.readJSON('./demo/checkout-MX.json'),
+        checkout_mx = file.readJSON('./demo/checkout-MX.json'),
         demo_data = file.readYAML('./demo/demo-data.yml'),
-        indexData = nitro.tools.scope({
+        index_data = nitro.tools.scope({
           dev: dev, pkg: pkg,
           git: {
             branch: process.env.DRONE_BRANCH || process.env.GIT_BRANCH || branch || require('git-rev-sync').branch()
@@ -114,69 +119,21 @@ module.exports = function (nitro) {
           },
           discountAmount: function (_article) {
             return checkout.order.total_amount - parseInt( checkout.order.articles.reduce(function (prev, article) {
-              return prev + article.quantity*indexData.articleAmount( article, true );
-            }, 0) + indexData.shippingAmount() );
+              return prev + article.quantity*index_data.articleAmount( article, true );
+            }, 0) + index_data.shippingAmount() );
           },
           totalAmount: function (_articles) {
             return checkout.order.total_amount;
           },
           amount2string: function (amount, decimalsSeparator, _groupSeparator) {
             var cents = amount%100;
-            return parseInt(amount/100) + decimalsSeparator + ( cents < 10 ? '0' : '' ) + cents;
+            return _separateThousands(parseInt(amount/100), _groupSeparator) + decimalsSeparator + ( cents < 10 ? '0' : '' ) + cents;
           },
           formatCurrency: function (amount, symbol) {
-            if( amount < 0 ) {
-              return '-' + indexData.formatCurrency(-amount, symbol);
-            }
-            if( symbol === 'EUR' ) return indexData.amount2string(amount, ',') + '€';
-            return '$' + indexData.amount2string(amount, '.');
-          }
-        }),
-        indexDataMX = nitro.tools.scope({
-          dev: dev, pkg: pkg,
-          git: {
-            branch: process.env.DRONE_BRANCH || process.env.GIT_BRANCH || branch || require('git-rev-sync').branch()
-          },
-          // dotcom: process.env.DRONE_BRANCH === 'release' || process.env.GIT_BRANCH === 'release' || require('git-rev-sync').branch() === 'release',
-          version: pkg.version + ( dev ? ( '-build' + new Date().getTime() ) : '' ),
-          build: Date.now(),
-          checkout: checkoutMX,
-          baseHref: '/',
-          shippingAmount: function () {
-            return (checkoutMX.shipping.price - checkoutMX.shipping.discount)*(1 + checkoutMX.shipping.tax_rate/10000);
-          },
-          articleAmount: function (article, discountApplied ) {
-            var price = article.price;
-            if( discountApplied && article.discount_rate ) {
-              price = price - ( article.price*article.discount_rate/10000 ) - (article.discount || 0);
-            }
-
-            return price*( 1 + article.tax_rate/10000 );
-          },
-          articleDiscount: function (article) {
-            if( !article.discount_rate ) {
-              return 0;
-            }
-            return ( article.price*article.discount_rate/10000 - (article.discount || 0) )*( 1 + article.tax_rate/10000 );
-          },
-          discountAmount: function (_article) {
-            return checkoutMX.order.total_amount - parseInt( checkoutMX.order.articles.reduce(function (prev, article) {
-              return prev + article.quantity*indexDataMX.articleAmount( article, true );
-            }, 0) + indexDataMX.shippingAmount() );
-          },
-          totalAmount: function (_articles) {
-            return checkoutMX.order.total_amount;
-          },
-          amount2string: function (amount, decimalsSeparator, _groupSeparator) {
-            var cents = amount%100;
-            return parseInt(amount/100) + decimalsSeparator + ( cents < 10 ? '0' : '' ) + cents;
-          },
-          formatCurrency: function (amount, symbol) {
-            if( amount < 0 ) {
-              return '-' + indexDataMX.formatCurrency(-amount, symbol);
-            }
-            if( symbol === 'EUR' ) return indexDataMX.amount2string(amount, ',') + '€';
-            return '$' + indexDataMX.amount2string(amount, '.');
+            console.log('formatCurrency', amount, symbol);
+            if( amount < 0 ) return '-' + index_data.formatCurrency(-amount, symbol);
+            if( symbol === 'EUR' ) return index_data.amount2string(amount, ',', '.') + '€';
+            return '$' + index_data.amount2string(amount, '.', ',');
           }
         });
 
@@ -190,7 +147,7 @@ module.exports = function (nitro) {
       file.write('public/demo-success.html', renderIndex( scope.new({ result: { closed: true, success: true } }) ) );
       file.write('public/demo-cancel.html', renderIndex( scope.new({ result: { closed: true, success: false } }) ) );
 
-    })(indexData.new({ country: 'ES', currency: 'EUR' }));
+    })(index_data.new({ country: 'ES', currency: 'EUR' }));
 
     (function (scope) {
 
@@ -200,15 +157,15 @@ module.exports = function (nitro) {
       file.write('public/mx/demo-success.html', renderIndex( scope.new({ result: { closed: true, success: true } }) ) );
       file.write('public/mx/demo-cancel.html', renderIndex( scope.new({ result: { closed: true, success: false } }) ) );
 
-    })(indexDataMX.new({ country: 'MX', currency: 'MXN' }));
+    })(index_data.new({ checkout: checkout_mx, country: 'MX', currency: 'MXN' }));
 
-    file.write('public/require.html', template( file.read('demo/require.html') )( indexData.new({ country: 'ES', currency: 'EUR', public_key: demo_data.public_key.demo_es }) ) );
+    file.write('public/require.html', template( file.read('demo/require.html') )( index_data.new({ country: 'ES', currency: 'EUR', public_key: demo_data.public_key.demo_es }) ) );
 
-    file.write('public/playground.html', template( file.read('demo/playground.html') )( indexData ) );
+    file.write('public/playground.html', template( file.read('demo/playground.html') )( index_data ) );
 
-    file.write('public/simulator/index.html', template( file.read('demo/demo-simulator.html') )( indexData.new({ baseHref: '/simulator/' }) ) );
+    file.write('public/simulator/index.html', template( file.read('demo/demo-simulator.html') )( index_data.new({ baseHref: '/simulator/' }) ) );
 
-    file.write('public/widgets.html', template( file.read('demo/widgets.html') )( indexData ) );
+    file.write('public/widgets.html', template( file.read('demo/widgets.html') )( index_data ) );
   });
 
   nitro.task('demo-dev', ['demo-lintjs:dev', 'demo-clear:dev', 'demo-assets:dev', 'demo-js:dev', 'demo-sass:dev', 'demo-templates:dev']);

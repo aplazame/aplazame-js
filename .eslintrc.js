ef3c740b14433238f92264e5c9d74c8eb859616a
Jesús Germade <jesus@germade.es>
2018-10-11 02:11:52 +0200

    eslint tweaks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,44 @@
 module.exports = {
-    "env": {
-      "browser": true,
-    },
-    "globals": {
-      "module": true,
-      "require": true,
-      "global": true,
-    },
-    "parserOptions": {
-      "sourceType": "module",
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-      /* TODO
-      "indent": [
-            "error",
-            2
-      ],
-      */
-      "no-empty": ["error", { "allowEmptyCatch": true }],
-      "no-console": 0,
-      "linebreak-style": [
+  "root": true,
+  "env": {
+    "browser": true,
+  },
+  "globals": {
+    "module": true,
+    "require": true,
+    "global": true,
+  },
+  "parserOptions": {
+    "sourceType": "module",
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    /* TODO
+    "indent": [
           "error",
-          "unix"
-      ],
-      "quotes": [
-          "error",
-          "single"
-      ],
-      "semi": [
-          "error",
-          "always"
-      ],
-      "no-unused-vars": [
-          "error",
-          {
-              "args": "all",
-              "argsIgnorePattern": "^_\\w+"
-          }
-      ]
-    }
+          2
+    ],
+    */
+    "no-empty": ["error", { "allowEmptyCatch": true }],
+    "no-console": 0,
+    "linebreak-style": [
+        "error",
+        "unix"
+    ],
+    "quotes": [
+        "error",
+        "single"
+    ],
+    "semi": [
+        "error",
+        "always"
+    ],
+    "no-unused-vars": [
+        "error",
+        {
+            "args": "all",
+            "argsIgnorePattern": "^_\\w+"
+        }
+    ]
+  }
 };

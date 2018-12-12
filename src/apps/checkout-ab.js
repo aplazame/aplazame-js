1db9762519ab19e849dcc689ead88dbd5edb0bab
Jesús Germade <jesus@germade.es>
2018-12-12 09:30:27 +0100

    rollforward checkout AB

--- src/apps/checkout-ab.js
+++ src/apps/checkout-ab.js
@@ -2,14 +2,14 @@
 import api from '../core/api';
 
 var _ab_list = [
-  // '6f9f8f04a55c4b50cb888e97c75eca4d82962f69',
-  // '78b9fe21619c6fc652b006fd1c772284bbd18f34',
-  // '12093289b594f50d3971e4719eedd5c314ceb6ba',
-  // '0e880ca5c85eb848ec878cb6d16dc223ea3ed2a8',
-  // '519727259c5d55599369d19df95f75a582a9e685',
-  // 'd78dcdcefe37c7b55cfa70ac48a4c8dc719e0875',
-  // '868ff632cfab95b8fec42a1e970a776686d8cf38',
-  // '73ee9ba32eab8893e7e07add3b133f706e95a761',
+  '6f9f8f04a55c4b50cb888e97c75eca4d82962f69',
+  '78b9fe21619c6fc652b006fd1c772284bbd18f34',
+  '12093289b594f50d3971e4719eedd5c314ceb6ba',
+  '0e880ca5c85eb848ec878cb6d16dc223ea3ed2a8',
+  '519727259c5d55599369d19df95f75a582a9e685',
+  'd78dcdcefe37c7b55cfa70ac48a4c8dc719e0875',
+  '868ff632cfab95b8fec42a1e970a776686d8cf38',
+  '73ee9ba32eab8893e7e07add3b133f706e95a761',
 ];
 
 export default function checkoutAB (checkout_url) {
